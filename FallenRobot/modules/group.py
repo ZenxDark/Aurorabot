8fc93e5fef61fadd2f9dd88f0053f3db60e301d9
ZenxDark <142667006+OnDrugs088@users.noreply.github.com>
2023-08-27 13:57:13 +0530

    Update group.py
    Signed-off-by: ZenxDark <142667006+OnDrugs088@users.noreply.github.com>

--- FallenRobot/modules/group.py
+++ FallenRobot/modules/group.py
@@ -3,7 +3,7 @@ __help__ = """
  ❍ /setgpic*:* As a reply to file or photo to set group profile pic!
  ❍ /delgpic*:* Same as above but to remove group profile pic.
  ❍ /setsticker*:* As a reply to some sticker to set it as group sticker set!
- ❍ /setdescription <description>*:* Sets new chat description in group.
+ ❍ /setdesc <description>*:* Sets new chat description in group.
 """
 
 __mod_name__ = "Gʀᴏᴜᴘ"
